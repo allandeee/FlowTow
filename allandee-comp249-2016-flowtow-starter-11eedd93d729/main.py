dcd8a338531b344b9c99f1c4776b3c979206dde4
allandeee <all4ndee@gmail.com>
2016-04-23 12:26:13 +1000

    Import CSS
    CSS and static files (images). Redirected paths

--- allandee-comp249-2016-flowtow-starter-11eedd93d729/main.py
+++ allandee-comp249-2016-flowtow-starter-11eedd93d729/main.py
@@ -2,7 +2,7 @@
 @author:
 '''
 
-from bottle import Bottle, template, debug
+from bottle import Bottle, template, debug, static_file
 import interface
 import users
 from database import COMP249Db
@@ -15,7 +15,12 @@ application = Bottle()
 @application.route('/')
 def index():
 
-    return template('index', title="Hello!")
+    return template('index', title="FlowTow!")
+
+
+@application.route('/static/<filename:path>')
+def static(filename):
+    return static_file(filename=filename, root='static')
 
 
 if __name__ == '__main__':
