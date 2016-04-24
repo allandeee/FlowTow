126667764c4e8572a7de48a3195d1b9bc1b8fbd9
allandeee <all4ndee@gmail.com>
2016-04-25 00:39:08 +1000

    corrections and part of level 1
    image path corrections. Progress on interface (list_images)

--- allandee-comp249-2016-flowtow-starter-11eedd93d729/interface.py
+++ allandee-comp249-2016-flowtow-starter-11eedd93d729/interface.py
@@ -2,15 +2,33 @@
 @author:
 '''
 
-import sqlite3
-
+import database
 
 def list_images(db, n, usernick=None):
     """Return a list of dictionaries for the first 'n' images in
     order of timestamp. Each dictionary will contain keys 'filename', 'timestamp', 'user' and 'likes'.
     The 'likes' value will be a count of the number of likes for this image as returned by count_likes.
     If usernick is given, then only images belonging to that user are returned."""
-
+    cur = db.cursor()
+    sql = """
+    select * from (
+    select * from images
+    order by timestamp DESC
+    )
+    limit %i;
+    """
+    cur.execute(sql % (n,))
+    img_list = (list(cur))
+    print(img_list)
+    dict_list = []
+    for i in img_list:
+        i_dict = dict()
+        i_dict['filename'] = i[0]
+        i_dict['timestamp'] = i[1]
+        i_dict['user'] = i[2]
+        i_dict['likes'] = count_likes(db, i_dict['filename'])
+        dict_list.append(i_dict)
+    print(dict_list)
 
 
 def add_image(db, filename, usernick):
@@ -22,22 +40,27 @@ def add_image(db, filename, usernick):
 
 def add_like(db, filename, usernick=None):
     """Increment the like count for this image"""
-    conn = sqlite3.connect(db)
-    cur = conn.cursor()
+    cur = db.cursor()
     sql = """
-    insert into likes values (%s, %s)
+    insert into likes values (%s, %s);
     """
     cur.execute(sql % (filename, usernick))
-    conn.commit()
+    db.commit()
 
 
 def count_likes(db, filename):
     """Count the number of likes for this filename"""
-    conn = sqlite3.connect(db)
-    cur = conn.cursor()
+    cur = db.cursor()
     sql = """
-    select count(filename) from likes where filename=%s
+    select count(filename) from likes [where filename=%s];
     """
     like_sum = cur.execute(sql % (filename,))
+    print(like_sum)
     return like_sum
 
+
+if __name__ == "__main__":
+    db = database.COMP249Db()
+    db.create_tables()
+    db.sample_data()
+    list_images(db, 3)
